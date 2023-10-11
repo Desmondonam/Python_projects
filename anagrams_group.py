b442be534e3d897f804ec6d804b25c46714d8225
Desmond Onam <desmondonam@gmail.com>
2023-10-11 18:12:19 +0300

    add anagrams code

---
+++ anagrams_group.py
@@ -0,0 +1,12 @@
+'''
+this python code is meant to group anagrams using python
+'''
+
+from collections import defaultdict
+
+def group_anagrams(a):
+    dfdict = defaultdict(list)
+    for i in a:
+        sorted_i = " ".join(sorted(i))
+        dfdict[sorted_i].append(i)
+    return dfdict.values()
