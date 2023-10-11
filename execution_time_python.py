fac98c4cdcfcf3afed4addf8899f5f010265472e
Desmond Onam <desmondonam@gmail.com>
2023-10-11 18:24:00 +0300

    add execution time python code

---
+++ execution_time_python.py
@@ -0,0 +1,18 @@
+'''
+THis is the code that is meant to calculate the execution time of a python code
+'''
+
+from time import time
+start = time()
+
+# Python program to create acronyms
+word = "Artificial Intelligence"
+text = word.split()
+a = " "
+for i in text:
+    a = a+str(i[0]).upper()
+print(a)
+
+end = time()
+execution_time = end - start
+print("Execution Time : ", execution_time)
